93f057bb6ebf60a4024dc146574558e6ee3678e3
evgenijspilka <seapp88@icloud.com>
2019-04-06 21:27:51 +0300

    Add socket server

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ global.Cells = require('./models/cell');
 io.on('connection', (socket) => {
     console.log('a user connected');
 
-    socket.emit('message', 'sfgsdgdfgsdgsdfgdsf')
+    socket.emit('turn:led', 'turn:led')
 
 });
 
